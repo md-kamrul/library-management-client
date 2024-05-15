f76ac63908e40c30e82c777b7a96bf899dbb824a
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-05-15 21:18:19 +0600

    BorrowedBooks.jsx file bug fix

--- src/Layouts/compoments/BorrowedBooks.jsx
+++ src/Layouts/compoments/BorrowedBooks.jsx
@@ -1,13 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import SingleBorrowedBook from "./SingleBorrowedBook";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const BorrowedBooks = () => {
 
     document.title = "NSU Library - Borrowed Books"
 
+    const userInfo = useContext(AuthContext);
+
     const all_books = useLoaderData();
-    console.log(all_books);
 
     const [borrowed_data, set_borrowed_data] = useState([]);
     const borrowCollectionURL = `https://library-management-server-pink.vercel.app/borrowBook`;
@@ -22,7 +24,7 @@ const BorrowedBooks = () => {
     let set_single_borrowed_book = [];
     for (let i = 0; i < all_books.length; i++) {
         for (let j = 0; j < borrowed_data.length; j++) {
-            if (all_books[i]._id == borrowed_data[j]._id) {
+            if (all_books[i]._id == borrowed_data[j]._id && borrowed_data[j].borrowedUserEmail === userInfo.user.email) {
                 set_single_borrowed_book[j] = all_books[i];
                 set_single_borrowed_book[j].borrowedDate = borrowed_data[j].borrowedDate;
                 set_single_borrowed_book[j].returnDate = borrowed_data[j].returnDate;
